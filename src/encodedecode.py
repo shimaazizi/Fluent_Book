d2759714e91ad77ce8a9059e36446232fbd07a52
shimaazizi <shima.azizimajd98@gmail.com>
2024-09-23 15:19:57 +0330

    add encode decode

--- src/encodedecode.py
+++ src/encodedecode.py
@@ -2,3 +2,14 @@ s = 'cafe'
 b = s.encode('utf8')
 print(b)
 print(b.decode('utf8'))
+
+# Error handling in unicode
+city = 'São Paulo'
+#city.encode('cp437')   (raise a UnicodeEncodeError)
+
+# The solution:
+print(city.encode('cp437', errors='ignore'))
+# or
+print(city.encode('cp437', errors='replace'))
+# or
+print(city.encode('cp437', errors='xmlcharrefreplace'))
